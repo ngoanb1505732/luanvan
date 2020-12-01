1a58f1bb2c44bebc505ee0e834bf756fb887f912
quang phuc <thqphuc97@gmail.com>
2020-12-01 22:38:29 +0700

    PhucTHQ fix js css

--- resources/views/frontend/template/index.blade.php
+++ resources/views/frontend/template/index.blade.php
@@ -31,30 +31,30 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/jquery/dist/jquery.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/jquery-bar-rating/dist/jquery.barrating.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/owl-carousel/owl.carousel.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/gmap3.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/imagesloaded.pkgd.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/isotope.pkgd.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/jquery.matchHeight-min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/slick/slick/slick.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/elevatezoom/jquery.elevatezoom.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/Magnific-Popup/dist/jquery.magnific-popup.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/jquery-bar-rating/dist/jquery.barrating.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/owl-carousel/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/gmap3.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/imagesloaded.pkgd.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/isotope.pkgd.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/jquery.matchHeight-min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/slick/slick/slick.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/elevatezoom/jquery.elevatezoom.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/Magnific-Popup/dist/jquery.magnific-popup.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/jquery-ui/jquery-ui.min.js"></script>
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAx39JFH5nhxze1ZydH-Kl8xXM3OK4fvcg&amp;region=GB"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/revolution/js/jquery.themepunch.tools.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}}/plugins/revolution/js/jquery.themepunch.revolution.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/revolution/js/extensions/revolution.extension.video.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
-    <script type="text/javascript" src="{{url("frontend")}}/plugins/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/revolution/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}}/plugins/revolution/js/jquery.themepunch.revolution.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/revolution/js/extensions/revolution.extension.video.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/plugins/revolution/js/extensions/revolution.extension.actions.min.js"></script>
     <!-- Custom scripts-->
-    <script type="text/javascript" src="{{url("frontend")}}/js/main.js"></script>
+    <script type="text/javascript" src="{{url("/")}}/js/main.js"></script>
 </head>
 <!--[if IE 7]><body class="ie7 lt-ie8 lt-ie9 lt-ie10"><![endif]-->
 <!--[if IE 8]><body class="ie8 lt-ie9 lt-ie10"><![endif]-->
